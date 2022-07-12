b10e7d46587475a802527cabd5715596dbd6fcc8
Hemant <hrsmart2150@gmail.com>
2022-07-12 13:29:45 +0530

    We have made our sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,10 +1,84 @@
-import React from 'react'
 import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faEnvelope, faSuitcase, faBagShopping } from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <div>
-        
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="Logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      </Link>
+
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+
+        <NavLink activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+
+        <NavLink activeclassname="active" className="portfolio-link" to="/portfolio">
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+
+      <ul>
+
+        <li>
+          <a
+            href="https://www.linkedin.com/in/hemantwasthere/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="https://github.com/Hemant-0000"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="https://twitter.com/hemantwasthere/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="https://drive.google.com/file/d/155K-57Tvm4Dc6FKsLo1O5aRFoZ58FOt6/view"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faBagShopping} color="#4d4d4e" />
+          </a>
+        </li>
+
+      </ul>
     </div>
   )
 }
