cec02d5b10e8353f686bef032e20c02c2516afc1
Hemant <hrsmart2150@gmail.com>
2022-07-17 19:10:20 +0530

    Optimized some code

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -83,7 +83,7 @@ const Contact = () => {
                 </div>
 
                 <div className="map-wrap">
-                    <MapContainer center={[28.695812, 77.072952]} zoom={15} >
+                    <MapContainer center={[28.690689, 77.070832]} zoom={15} >
                         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                         <Marker position={[28.695812, 77.072952]}>
                             <Popup>Hemant lives here, come over for a cup of coffee :)</Popup>
